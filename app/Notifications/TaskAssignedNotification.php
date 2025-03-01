bb120e3acdda1f79e1922d3da939315f959ec9f3
danh126 <=>
2025-03-01 08:50:53 +0700

    final

--- app/Notifications/TaskAssignedNotification.php
+++ app/Notifications/TaskAssignedNotification.php
@@ -26,11 +26,11 @@ class TaskAssignedNotification extends Notification implements ShouldQueue
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-                    ->subject('Bạn có nhiệm vụ mới!')
-                    ->greeting('Xin chào, '.$notifiable->name)
-                    ->line('Bạn vừa được giao nhiệm vụ mới: '. $this->task->title)
-                    ->action('Xem chi tiết', url('http://127.0.0.1:8000/spa/tasks'))
-                    ->line('Vui lòng kiểm tra và thực hiện nhiệm vụ!');
+            ->subject('Bạn có nhiệm vụ mới!')
+            ->greeting('Xin chào, ' . $notifiable->name)
+            ->line('Bạn vừa được giao nhiệm vụ mới: ' . $this->task->title)
+            ->action('Xem chi tiết', url(env('APP_URL', 'http://127.0.0.1:8000') . '/spa/tasks'))
+            ->line('Vui lòng kiểm tra và thực hiện nhiệm vụ!');
     }
 
     public function toArray(object $notifiable): array
